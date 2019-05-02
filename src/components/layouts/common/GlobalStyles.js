2210e92b0761b34d7dca446da12c4959fce4d9a3
Flynn Buckingham <dev@flynnbuckingham.com>
2019-05-02 18:06:04 -0400

    Switched to prefer Open Sans font
    Signed-off-by: Flynn Buckingham <dev@flynnbuckingham.com>

--- src/components/layouts/common/GlobalStyles.js
+++ src/components/layouts/common/GlobalStyles.js
@@ -6,12 +6,13 @@ const normalizeStyles = normalize();
 
 const globalStyles = css`
 	body {
-		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+		font-family: 'Open Sans', 'Franklin Gothic Medium', Arial, sans-serif;
 	}
 `;
 
 export default () => (
 	<Fragment>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700" rel="stylesheet" />
 		<Global styles={normalizeStyles} />
 		<Global styles={globalStyles} />
 	</Fragment>
